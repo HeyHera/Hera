00c962e171a4f3fcbdde58dd81f007439d6ede14
Arjun Vishnu Varma <62504826+Arjun-Varma2@users.noreply.github.com>
2022-05-09 11:00:56 +0530

    Added function

--- tts/espeak.py
+++ tts/espeak.py
@@ -1,17 +1,18 @@
 import pyttsx3
+
 # initialize Text-to-speech engine
-engine = pyttsx3.init()
-# initialize Text-to-speech engine
-engine = pyttsx3.init()
+
+def tts(text): 
+	engine = pyttsx3.init()
 # convert this text to speech
 #text = "Good morning Arjun"
-text = "How are you Arjun, nandu, gokul and jithin"
-engine.setProperty("rate", 105)
+#text = "How are you Arjun, nandu, gokul and jithin"
+	engine.setProperty("rate", 105)
 # checking voices
-voices = engine.getProperty("voices")
+	voices = engine.getProperty("voices")
 # set another voice
 #engine.setProperty("voice", voices[10].id)
-engine.setProperty('voice', 'english_rp+f3') #female
+	engine.setProperty('voice', 'english_rp+f3') #female
 #engine.setProperty('voice', 'com.apple.speech.synthesis.voice.samantha')
-engine.say(text)
-engine.runAndWait()
+	engine.say(text)
+	engine.runAndWait()
