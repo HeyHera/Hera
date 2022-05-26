847c1b2236902b62debdd726500c643dda4c8255
gokulmanohar <gokulmanohar7@gmail.com>
2022-05-26 23:23:48 +0530

    TTS init run from main

--- text-to-speech/espeak.py
+++ text-to-speech/espeak.py
@@ -1,29 +1,27 @@
 import pyttsx3
 
 # initialize Text-to-speech engine
+try:
+    # Comment out these lines if you don't want sound output
+    engine = pyttsx3.init()
+    print("\n{} Text To Speech engine initialized {}".format(
+        '='*20, '='*20))
+    engine.setProperty("rate", 140)
+    # -------------------------
+    # checking voices
+    # voices = engine.getProperty("voices")
+    # set another voice
+    # engine.setProperty("voice", voices[10].id)
+    # -------------------------
+    engine.setProperty('voice', 'english_rp+f3')  # female
+except Exception as e:
+    print("\nAn error occurred while initializing Text To Speech engine.\n" + str(e))
 
 
 def tts(text):
-    try:
-        print("\n" + text)
-        # Comment out these lines if you don't want sound output
-
-        engine = pyttsx3.init()
-        print("\n{} Text To Speech engine initialized{}".format(
-            '='*20, '='*20))
-        engine.setProperty("rate", 140)
-        # -------------------------
-        # checking voices
-        # voices = engine.getProperty("voices")
-        # set another voice
-        # engine.setProperty("voice", voices[10].id)
-        # -------------------------
-        engine.setProperty('voice', 'english_rp+f3')  # female
-        engine.say(text)
-        engine.runAndWait()
-
-    except Exception as e:
-        print("\nAn error occurred while initializing Text To Speech engine.\n" + str(e))
+    print("\n" + text)
+    engine.say(text)
+    engine.runAndWait()
 
 
 if __name__ == '__main__':
