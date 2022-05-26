2219f8c4730aae43bfe22fc426fe8ea7d739338f
gokulmanohar <gokulmanohar7@gmail.com>
2022-05-26 23:22:38 +0530

    Add new frame works spacy and rasa

--- test.py
+++ test.py
@@ -1,10 +1,29 @@
-from sympy import re
+# pip install -U spacy
+# python -m spacy download en_core_web_sm
+import spacy
 
+# Load English tokenizer, tagger, parser and NER
+nlp = spacy.load("en_core_web_sm")
 
-flag = 0
-def change():
-    global flag
-    flag = 1
+# Process whole documents
+text = ("Play the song magic")
+text = ("open the application terminal")
+text = ("Play the song a song")
+doc = nlp(text)
 
-change()
-print(flag)
+# Analyze syntax
+print("Noun phrases:", [chunk.text for chunk in doc.noun_chunks])
+print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
+
+# Find named entities, phrases and concepts
+for entity in doc.ents:
+    print(entity.text, entity.label_)
+
+print("-"*80)
+for x in doc :
+    if x.pos_ == "NOUN":
+        print("Noun", x)
+    if x.pos_ == "PROPN":
+        print("Proper Noun", x)
+    if x.pos_=="PRON":
+        print("Pronoun", x)
