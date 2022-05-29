be8f50d58f024dcc8e3f5eb8d486b5725d951b80
gokulmanohar <gokulmanohar7@gmail.com>
2022-05-29 20:21:13 +0530

    modified entity_extractor.py

--- nlu/entity_extraction/entity_extractor.py
+++ nlu/entity_extraction/entity_extractor.py
@@ -1,15 +1,15 @@
 import spacy
-
-model_test = "would you be pleased if i asked you to open the application google chrome"
-
-# load the trained model
 nlp_output = spacy.load("nlu/entity_extraction/output/model-best")
 
-# pass our test instance into the trained pipeline
-doc = nlp_output(model_test)
+def extract(model_test_sentence, entity_label):
+    doc = nlp_output(model_test_sentence)
+    # print out the identified entities
+    for ent in doc.ents:
+        if ent.label_ == entity_label:
+            return(ent.text)
 
-
-# print out the identified entities
-for ent in doc.ents:
-    if ent.label_ == "APPLICATION":
-        print(ent.text, ent.label_)
+if __name__ == '__main__':
+    model_test_sentence = "would you be pleased if i asked you to open the application google chrome"
+    entity_label = "APPLICATION"
+    pt = extract(model_test_sentence, entity_label)
+    print(pt)
