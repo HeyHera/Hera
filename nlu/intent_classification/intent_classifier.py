df91955d78b3ce9e624823c01ee71299195a28b4
gokulmanohar <gokulmanohar7@gmail.com>
2022-06-24 19:59:49 +0530

    add comments

--- nlu/intent_classification/intent_classifier.py
+++ nlu/intent_classification/intent_classifier.py
@@ -6,12 +6,12 @@ clf = load('nlu/intent_classification/models/sklearn_intent_classifier.joblib')
 
 def classify(str):
     input_list = []*1
-    # type(str) >> string
+    # type(str) = string
     input_list.insert(0, str)
     predicted_intents = clf.predict(input_list)
     return(predicted_intents[0])
 
 
 if __name__ == '__main__':
-    pt = classify('delete the latest file in downloads')
+    pt = classify('who are the americans')
     print(pt)
