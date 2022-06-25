3b1981f8055397401e7294b220c30c969610860f
gokulmanohar <gokulmanohar7@gmail.com>
2022-06-25 16:03:23 +0530

    kivy instantaneous value change

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from audioop import mul
+import time
 from kivy.lang import Builder
 from kivymd.app import MDApp
-import random   
+import multiprocessing
+import skill_func as skill
 
 
 KV = '''
@@ -19,7 +22,7 @@ MDScreen:
         pos_hint: {"center_x": 0.5, "center_y": 0.25}
         theme_text_color: "Custom"
         line_color: get_color_from_hex("#f2dafe")
-        on_press: app.change_word()
+        on_press: app.script()
 '''
 class Demo(MDApp):
 
@@ -32,10 +35,34 @@ class Demo(MDApp):
         screen =  Builder.load_string(KV)
         return screen
 
-    def change_word(self): #What Parameters should I give after self?
-            site_list=['Google','Yahoo','Microsoft','APKpure','APKMB','Stackoverflow','Bing']
-            text=random.choice(site_list)
-            self.root.ids.text_update.text=(text)
+    def change_word(self, r_msg): #What Parameters should I give after self?
+        print("Got request to change word")
+        self.root.ids.text_update.text=(r_msg)
+        return(0)
+
+    
+    def script(self):
+        msg = "None"
+        pipe_begin, pipe_end = multiprocessing.Pipe()
+        p1 = multiprocessing.Process(target=skill.sender, args=(pipe_end,))
+        p1.start()
+        msg = pipe_begin.recv()
+        pipe_begin.close()
+        print("Received the message: {}".format(msg))
+        if msg != "None":
+            print("Requesting to change word")
+            chn_wrd = self.change_word(msg)
+            # if chn_wrd == 0:
+            #     self.change_word("...")
+            # print("finished")
+            # change_word_process = multiprocessing.Process(target=self.change_word, args=(msg,))
+            # change_word_process.start()
+            # change_word_process.join()
+        # p1.join()
+        # msg = "None"
+        # self.change_word(msg)
+
+
 
 
 if __name__ == "__main__":
--- skill_func.py
+++ skill_func.py
@@ -1,11 +1,20 @@
+import random
+import subprocess
 import time
 
 def sender(pipe_end):
     """
     function to send messages to other end of pipe
     """
-    msg = "Playing the song Faded"
+    site_list=['Google','Yahoo','Microsoft','APKpure','APKMB','Stackoverflow','Bing']
+    msg=random.choice(site_list)
+    # msg = "Playing the song Faded"
     pipe_end.send(msg)
     print("Sent the message: {}".format(msg))
     pipe_end.close()
-    time.sleep(5.0)
+    print("Sleeping 5 sec")
+    vlc_path = "/usr/bin/vlc"
+    time.sleep(5)
+    subprocess.call([vlc_path, '../Music/sample.mp3'], stderr=subprocess.STDOUT) 
+
+    print("Sleep finished")
