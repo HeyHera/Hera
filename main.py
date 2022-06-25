e20226e0403f1381f656b4eaffc75a411a028c9e
gokulmanohar <gokulmanohar7@gmail.com>
2022-06-25 14:09:22 +0530

    functioning label changer

--- main.py
+++ main.py
@@ -1,68 +1,42 @@
-# from kivymd.app import MDApp
-# from kivymd.uix.screen import MDScreen
-# from kivymd.uix.button import MDRectangleFlatButton
-
-
-# class MainApp(MDApp):
-#     def build(self):
-#         self.theme_cls.theme_style = "Dark"  # "Light"
-#         screen = MDScreen()
-#         screen.add_widget(
-#             MDRectangleFlatButton(
-#                 text="Hello, World",
-#                 pos_hint={"center_x": 0.5, "center_y": 0.5},
-#             )
-#         )
-#         return screen
-
-
-# MainApp().run()
-
+from kivy.lang import Builder
 from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel
-from kivymd.uix.screen import MDScreen
-msg = "Playing the song Faded"
+import random   
+
+
+KV = '''
+#:import get_color_from_hex kivy.utils.get_color_from_hex
+MDScreen:
+    MDLabel:
+        id: text_update
+        text: "Hello!"
+        halign: "center"
+        theme_text_color: "Custom"
+        text_color: get_color_from_hex("#f2dafe")
+        font_style: 'H2'
+
+    MDRectangleFlatButton:
+        text: "ACTIVATE"
+        pos_hint: {"center_x": 0.5, "center_y": 0.25}
+        theme_text_color: "Custom"
+        line_color: get_color_from_hex("#f2dafe")
+        on_press: app.change_word()
+'''
 class Demo(MDApp):
 
+    def build(self):
 
-	def build(self):
+        # defining screen
+        self.theme_cls.theme_style = "Dark"
+        # self.msg = StringProperty()
+        # self.msg = "Hello!"
+        screen =  Builder.load_string(KV)
+        return screen
 
-		# defining screen
-		screen = MDScreen()
-		self.theme_cls.theme_style = "Dark"
-
-		# defining 1st label
-		l = MDLabel(text="Welcome!", pos_hint={'center_x': 0.8,
-											'center_y': 0.8},
-					theme_text_color="Custom",
-					text_color=(0.5, 0, 0.5, 1),
-					font_style='Caption')
-
-		# defining 2nd label
-		# l1 = MDLabel(text=msg, pos_hint={'center_x': 0.5,
-		# 								'center_y': 0.5},
-		# 			theme_text_color="Custom",
-		# 			text_color=(0.7, 0, 0.4, 1),
-		# 			font_style='H2')
-
-		# defining 3rd label
-		# l2 = MDLabel(text="Welcome!", pos_hint={'center_x': 0.8,
-		#                                         'center_y': 0.2},
-		#              theme_text_color="Custom",
-		#              text_color=(0.5, 0, 0.5, 1),
-		#              font_style='H1')
-		display_label = MDLabel(text=msg.title(),
-					halign="center",
-					theme_text_color="Custom",
-					text_color=(0.5, 0, 0.5, 1),
-					font_style='H2')
-
-		screen.add_widget(display_label)
-
-		# screen.add_widget(l1)
-		# screen.add_widget(l2)
-		return screen
+    def change_word(self): #What Parameters should I give after self?
+            site_list=['Google','Yahoo','Microsoft','APKpure','APKMB','Stackoverflow','Bing']
+            text=random.choice(site_list)
+            self.root.ids.text_update.text=(text)
 
 
 if __name__ == "__main__":
-	Demo().run()
+    Demo().run()
