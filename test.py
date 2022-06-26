c02e2f34aab7ecc5b690c3af7e2990e3852f2b8b
gokulmanohar <gokulmanohar7@gmail.com>
2022-06-26 10:24:25 +0530

    add material design to working code

--- test.py
+++ test.py
@@ -1,34 +1,24 @@
-import random
-import subprocess
+import threading
 import time
 from kivymd.app import MDApp
 from kivy.lang import Builder
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, ObjectProperty
 from kivymd.uix.screen import MDScreen
 from kivy.clock import Clock
 
-# LOCAL IMPORTS
-try:
-    # import wake_word_detection.wake_word_detection_script as wwd_module
-    import automatic_speech_recognition.automatic_speech_recognition_script as asr_module
-    import nlu.intent_classification.intent_classifier as intent_classifier_module
-    import tts.speak as tts_module
-    import skills.greetings as greeting_skill
-    import skills.music_playback as music_playback_skill
-    import skills.launch_application as launch_application_skill
-    import skill_func
-    print("\nLocal imports successful")
-except Exception as e:
-    print("\nLocal imports unsuccessful.\n" + str(e))
+
+class GV:
+    global_label_text = ""
 
 
 Builder.load_string("""
 #:import get_color_from_hex kivy.utils.get_color_from_hex
 <MySec>:
+    status: status_label
     MDScreen:
         MDLabel:
-            id: text_update
-            text: root.seconds_string
+            id: status_label
+            text: "Hello!"
             halign: "center"
             theme_text_color: "Custom"
             text_color: get_color_from_hex("#f2dafe")
@@ -39,40 +29,48 @@ Builder.load_string("""
             pos_hint: {"center_x": 0.5, "center_y": 0.25}
             theme_text_color: "Custom"
             line_color: get_color_from_hex("#f2dafe")
-            on_release: app.script()
+            on_release: root.display_hello_status()
 """)
 
 
 class MySec(MDScreen):
-    seconds_string = StringProperty('Hello!')
+    status = ObjectProperty(None)
+
+    def display_hello_status(self):
+        threading.Thread(target=self.do_something).start()
+
+    def do_something(self):
+        # First pass
+        print('starting first')
+        time.sleep(7)
+        GV.global_label_text = "Play a song"
+        # schedule the GUI update back on the main thread
+        Clock.schedule_once(self.something_finished)
+
+        # Second pass
+        print('starting second')
+        time.sleep(7)
+        GV.global_label_text = "Playing a random song"
+        print('finished second')
+        # schedule the GUI update back on the main thread
+        Clock.schedule_once(self.something_finished)
+
+        # schedule the GUI update to clear label back on the main thread
+        time.sleep(3)
+        Clock.schedule_once(self.clear_label)
+
+    def something_finished(self, dt):
+        print(GV.global_label_text)
+        self.status.text = GV.global_label_text
+
+    def clear_label(self, dt):
+        self.status.text = "..."
 
 
 class Hera(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
-        Clock.schedule_interval(lambda dt: self.script(), 1)
         return MySec()
-
-    def script(self):
-        # site_list=['Google','Yahoo','Microsoft','APKpure','APKMB','Stackoverflow','Bing']
-        # msg=random.choice(site_list)
-        # self.root.seconds_string = msg
-        # site_list=['2Google','2Yahoo','2Microsoft','2APKpure','2APKMB','2Stackoverflow','2Bing']
-        # msg=random.choice(site_list)
-        # self.root.seconds_string = msg
-        try:
-            print("\n{} Automatic Speech Recognition initializing {}".format(
-                '='*20, '='*20))
-            spoken = asr_module.asr()
-            spoken = str(spoken).lower()
-            print(spoken)
-            # self.change_word(spoken)
-            self.seconds_string = spoken
-        except Exception as e:
-            print(
-                "\nError encountered. Couldn't connect with Automatic Speech Recognition.\n" + str(e))
-        spoken = asr_module.asr()
-        self.seconds_string = "spoken"
 
 
 if __name__ == '__main__':
